65ed70310d01cc387f1a2ced4917b337857def43
Viktorian76 <pervemunu@gmail.com>
2024-07-13 21:33:55 +0300

    change: consumer work variant

--- src/consumer2.py
+++ src/consumer2.py
@@ -9,9 +9,10 @@ def main():
     channel.queue_declare(queue=name_queue)
 
     def callback(ch, method, properties, body):
-        pass
+        print(f" [x] Received {body}")
 
     channel.basic_consume(queue=name_queue, on_message_callback=callback, auto_ack=True)
+    print(" [*] Waiting for messages. To exit press CTRL+C")
     channel.start_consuming()
 
 
