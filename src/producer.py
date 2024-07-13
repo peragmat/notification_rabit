7701698d2a06fe97b5a4a76234b419497dc6e869
Viktorian76 <pervemunu@gmail.com>
2024-07-13 21:34:20 +0300

    add: basic_producer

---
+++ src/producer.py
@@ -0,0 +1,10 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+channel = connection.channel()
+
+channel.queue_declare(queue="second")
+
+channel.basic_publish(exchange="", routing_key="hello", body="Hello World!")
+print(" [x] Sent 'Hello World!'")
+connection.close()
